16b03c5536c1ac9dcecced47d60c4071932dcb8d
Zmonne <zmonne@outlook.com>
2020-10-16 22:24:51 -0300

    Inclusão do Scrip3, e primeiro ciclo de revisão.

---
+++ Script3.groovy
@@ -0,0 +1,41 @@
+byte a = 50
+byte b = 25
+byte c
+
+c = a + b
+
+println c
+
+//byte
+println Byte.MIN_VALUE
+println Byte.MAX_VALUE
+//short
+println Short.MIN_VALUE
+println Short.MAX_VALUE
+//int
+println Integer.MIN_VALUE
+println Integer.MAX_VALUE
+//long
+println Long.MIN_VALUE
+println Long.MAX_VALUE
+//float
+println Float.MIN_VALUE
+println Float.MAX_VALUE
+//double
+println Double.MIN_VALUE
+println Double.MAX_VALUE
+
+//char
+/* char apenas um caracter simples*/
+char teste1 = 'a'
+println teste1
+
+//boolean
+//string
+
+def teste3 = (byte)10
+println teste3
+println teste3.getClass().getName()
+
+def teste4 = 10
+println teste4.getClass().getName()
