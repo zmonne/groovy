2c0702644213654de3fa5cb2b359a6b07f038dc2
Zmonne <zmonne@outlook.com>
2020-10-16 22:26:40 -0300

    Atualização do Script3.

--- Script3.groovy
+++ Script3.groovy
@@ -1,34 +1,35 @@
+//byte
+
 byte a = 50
 byte b = 25
 byte c
 
 c = a + b
 
-println c
+println c + '\n'
 
-//byte
 println Byte.MIN_VALUE
-println Byte.MAX_VALUE
+println Byte.MAX_VALUE + '\n'
 //short
 println Short.MIN_VALUE
-println Short.MAX_VALUE
+println Short.MAX_VALUE + '\n'
 //int
 println Integer.MIN_VALUE
-println Integer.MAX_VALUE
+println Integer.MAX_VALUE + '\n'
 //long
 println Long.MIN_VALUE
-println Long.MAX_VALUE
+println Long.MAX_VALUE + '\n'
 //float
 println Float.MIN_VALUE
-println Float.MAX_VALUE
+println Float.MAX_VALUE + '\n'
 //double
 println Double.MIN_VALUE
-println Double.MAX_VALUE
+println Double.MAX_VALUE + '\n'
 
 //char
 /* char apenas um caracter simples*/
-char teste1 = 'a'
-println teste1
+char teste1 = '&'
+println teste1 + '\n'
 
 //boolean
 //string
